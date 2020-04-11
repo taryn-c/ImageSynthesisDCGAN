7c20341b97e2879612ba49df3e322fbabeb70401
tarync <tarync@sfu.ca>
2020-04-10 20:39:08 -0700

    Add one sided label smoothing to discriminator loss

--- DCGAN_local.py
+++ DCGAN_local.py
@@ -245,7 +245,7 @@ This method quantifies how well the discriminator is able to distinguish real im
 """
 
 def discriminator_loss(real_output, fake_output):
-    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    real_loss = cross_entropy(tf.fill(real_output, 0.9), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
     return total_loss
